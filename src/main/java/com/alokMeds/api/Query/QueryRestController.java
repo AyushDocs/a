9b325ff8719b4f89df39753d08f4814e872d1e1b
ayush <notTelling@gmail.com>
2021-09-24 23:56:27 +0300

    front and back work

--- src/main/java/com/alokMeds/api/Query/QueryRestController.java
+++ src/main/java/com/alokMeds/api/Query/QueryRestController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,18 +21,14 @@ public class QueryRestController{
     @Autowired
     private QueryRepository queryRepository;
    @PostMapping("/")
-   public Query save(@RequestBody QueryRecieved[] queryParam){
+   @CrossOrigin(value="*",allowedHeaders = "*")
+   public Query save(@RequestBody QueryRecieved queryParam[]){
       Query q=QueryRecieved.queryRecievedToQuery(queryParam[0]);
       queryRepository.save(q);
       return queryRepository.findByEmailAndQuery(q.getEmail(),q.getQuery());
    }
-    //@PostMapping("/")
-    //public Query save(@RequestBody Map<String,Object> map){
-    //    Query q=new Query((String)map.get("email"),(String)map.get("query"));
-    //    queryRepository.save(q);
-    //    return queryRepository.findByEmailAndQuery(q.getEmail(),q.getQuery());
-    //}
     @GetMapping("/")
+    @CrossOrigin(value="*",allowedHeaders = "*")
     public Page<Query> findAll(@RequestParam Optional<Integer> offset,@RequestParam Optional<Integer> size,
     @RequestParam Optional<String> sortBy){
      return queryRepository.findWithPagination(offset, size, sortBy);
@@ -43,15 +40,12 @@ public class QueryRestController{
     queryRepository.save(q);
     }
     @DeleteMapping("/{id}")
+    @CrossOrigin(value="*",allowedHeaders = "*")
     public void delete(@PathVariable String id){
         queryRepository.deleteById(id);
     }
     @GetMapping("/id/{id}")
-    public void findById(@PathVariable String id){
-        queryRepository.findById(id);
-    }
-    @GetMapping("/trial")
-    public QueryRecieved tri(){
-       return new QueryRecieved("email","query");
+    public Query findById(@PathVariable String id){
+      return  queryRepository.findById(id).orElseThrow(RuntimeException::new);
     }
 }
