ec1076265ff242e7c5a648afca6629d754ccedef
ayush <notTelling@gmail.com>
2021-09-29 11:24:52 +0300

    admin ready

--- src/main/frontend 2/src/components/About.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function About(props) {  
-return (
-    <div>    
-    {/* {props.textAndHeadings
-        .map((element)=>{return(
-        <AboutItem heading={element.heading} text1={element.text1} text2={element.text2}/>
-        )})} */}
-    </div>)}
